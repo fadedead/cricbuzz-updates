ec952052b2df6b202b6913700949500ee5f02c15
fadedead <fadedeadd@gmail.com>
2024-05-04 15:05:04 +0530

    Added: Fetching data and pushing to db for old matches

---
+++ constants.py
@@ -0,0 +1,13 @@
+"""
+Constants
+"""
+
+# Cricbuzz
+CB_MATCH_BASE_URL = "https://www.cricbuzz.com/api/cricket-match/commentary"
+CB_COMMENTARY_PAGINATION_BASE_URL = (
+    "https://www.cricbuzz.com/api/cricket-match/commentary-pagination"
+)
+
+# Fetcher
+GAME_LIST_FETCHER_SLEEP_TIME = 600
+GAME_UPDATE_FETCHER_SLEEP_TIME = 1
